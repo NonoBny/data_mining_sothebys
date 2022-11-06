c32f58ea496e3ed8b6574aabc97b8d95acd7c84b
Yosef Schoen <josephaschoen@gmail.com>
2022-11-06 16:40:25 +0200

    Added my path successfully

--- new_page_scrapping.py
+++ new_page_scrapping.py
@@ -5,6 +5,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import StaleElementReferenceException
 import requests
 from bs4 import BeautifulSoup
 
@@ -20,41 +22,59 @@ def get_path():
         return _path
 
 
+def log_in_authentication():
+    WebDriverWait(driver, 20).until(
+        EC.element_to_be_clickable((By.XPATH, "//div[@class='LinkedText']//a[text()='Log In']"))).click()
+    WebDriverWait(driver, 20).until(
+        EC.element_to_be_clickable((By.XPATH, "//div[@class='form-input-row ']//input[@id='email']"))).send_keys(
+        "noabenayoun1998@gmail.com")
+    WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH,
+                                                                "//div[@class='form-input-row-password \n                        ']//input[@id='password']"))).send_keys(
+        "itcnbVC5678")
+    WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//button[@id='login-button-id']"))).click()
+
+
+# each of our paths work differently
 path = get_path()
 if path != '':
     driver = webdriver.Chrome(path)
 else:
     driver = webdriver.Chrome()
+
 # Optional argument,
 # if not specified will search path.
-
 driver.get('https://www.sothebys.com/en/buy/auction/2022/monochrome-important-chinese-art')
 
-# login in authentification
-WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//div[@class='LinkedText']//a[text()='Log In']"))).click()
-WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//div[@class='form-input-row ']//input[@id='email']"))).send_keys("noabenayoun1998@gmail.com")
-WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//div[@class='form-input-row-password \n                        ']//input[@id='password']"))).send_keys("itcnbVC5678")
-WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//button[@id='login-button-id']"))).click()
+# login in authentication will call the login function
+log_in_authentication()
 
 # access the list of results
+hoverable_header = WebDriverWait(driver, 20)\
+    .until(EC.element_to_be_clickable((By.XPATH, "//div[@class='PageHeader-body']")))
 
-hoverable_header = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//div[@class='PageHeader-body']")))
 ActionChains(driver)\
         .move_to_element(hoverable_header)\
         .perform()
-hoverable_auction = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//div[@class='PageHeader-body']//div[@class='NavigationItemTitle']//span[text()='Auctions']")))
+hoverable_auction = WebDriverWait(driver, 20)\
+    .until(EC.element_to_be_clickable((By.XPATH, "//div[@class='PageHeader-body']//div[@class='NavigationItemTitle']//span[text()='Auctions']")))
+
 ActionChains(driver)\
         .move_to_element(hoverable_auction)\
         .perform()
 
-WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//div[@class='SothebysTopNavigationItem']//div[@class='NavigationLink']//a[text()='Results']"))).click()
+WebDriverWait(driver, 20)\
+    .until(EC.element_to_be_clickable((By.XPATH, "//div[@class='SothebysTopNavigationItem']//div[@class='NavigationLink']//a[text()='Results']"))).click()
 
 different_sales = driver.find_elements(By.CLASS_NAME, "Card-info-container")
 print(len(different_sales))
-
 for different_sale in different_sales:
-    WebDriverWait(driver, 20).until(EC.element_to_be_clickable(different_sale)).click()
+    print(different_sale.text)
+ignored_exceptions = (NoSuchElementException, StaleElementReferenceException,)
+for different_sale in different_sales:
+    WebDriverWait(driver, 20, ignored_exceptions=ignored_exceptions)\
+        .until(EC.element_to_be_clickable(different_sale)).click()
     driver.back()
+    print("testing\n")
 
 #print([my_elem.get_attribute("textContent") for my_elem in driver.find_elements(By.CLASS_NAME, "SearchModule-results-item")])
 
