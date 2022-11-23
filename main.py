e458ed193910697743b28aee29f23414cf719846
Noa Benayoun <noabenayoun1998@gmail.com>
2022-11-23 14:40:24 +0200

    hello

--- main.py
+++ main.py
@@ -89,7 +89,9 @@ def get_url_n_sale_total():
     list_url = []
     list_sale_total = []
     sale_items = soup.find_all('a', class_='Card-info-container', href=True)
+
     for sale_item in sale_items:
+
         if AUCTION_LINK in sale_item['href']:
             list_url.append(str(sale_item['href']))
             total_sale_list = sale_item.find("div", class_="Card-salePrice")
@@ -118,7 +120,8 @@ def general_info():
         "//*[@id='__next']/div/div[4]/div/div[1]/div[4]/div/div[2]/div/p[1]/text()[2]")
     location_auction = documentobjectmodel.xpath("//*[@id='__next']/div/div[4]/div/div[1]/div[4]/div/div[2]/div/p[2]")
 
-    get_info_string = lambda item: 'n/a' if item is None else item[0].text
+    def get_info_string(item):
+        return 'n/a' if item is None else item[0].text
 
     str_date = get_info_string(date_auction)
     str_loc = get_info_string(location_auction)
@@ -136,16 +139,17 @@ def item_or_art_display_list(sale_item):
     """check if the auction is of art pieces (with an author) or antiques/objects etc
     because data points are not the same in this case (no author) and return the specific data points
     if the display is in list form"""
-    # this is when it is an art production (3 data points)
+
     if sale_item.find("p", class_="css-1o7cmk8"):
         item_obj = sale_item.find("p", class_="css-1o7cmk8")
+
         if item_obj is not None:
             info_title = item_obj.text.split(maxsplit=1)
             index_item = info_title[0][:-1]
             title_item = info_title[1]
             type_item = "Other items"
         return index_item, title_item, type_item
-    # this is when it is an object (2 data points)
+
     if sale_item.find("div", class_="css-wdkl43"):
         author_and_index = sale_item.find("p", class_="css-8908nx")
         title_art = sale_item.find("p", class_="css-17ei96f")
@@ -162,10 +166,10 @@ def item_or_art_display_square(sale_item):
     """check if the auction is of art pieces (with an author) or antiques/objects etc
         because data points are not the same in this case (no author) and return the specific data points
         if the display is in square form"""
-    # this is when it is an art production (3 data points)
     if sale_item.find("p", class_="paragraph-module_paragraph16Regular__CXt6G css-5dbuiq"):
         author_and_index = sale_item.find("h5", class_="headline-module_headline20Regular__zmXrx css-y1q8mr")
         title_art = sale_item.find("p", class_="paragraph-module_paragraph14Regular__Zfr98 css-17r6vaq")
+
         if author_and_index is not None and title_art is not None:
             author_and_index = author_and_index.text.split(maxsplit=1)
             index = author_and_index[0][:-1]
@@ -173,7 +177,6 @@ def item_or_art_display_square(sale_item):
             title_of_artpiece = title_art.text
             type_item = "Art pieces"
         return index, title_of_artpiece, author, type_item
-    # this is when it is an object (2 data points)
     else:
         item_obj = sale_item.find("h5", class_="headline-module_headline20Regular__zmXrx css-1t5w3xl")
         if item_obj is not None:
@@ -199,10 +202,8 @@ def length_type_type(type_item, count_dict, estimate_price_str, price_number, pr
         count_dict[type_item[3]] += 1
         new_dict = {"Title of Item": type_item[1], "Author": type_item[2], "Estimated Price": estimate_price_str,
                     "Selling price": price_number, "Currency": price_currency, "Reserve": reserve_or_not}
-    # check if it is an item (with 2 data points title and index)
     elif len(type_item) == 3:
         count_dict[type_item[2]] += 1
-        #collect_dict["Type of Items"] = type_item[2]
         new_dict = {"Title of Item": type_item[1], "Estimated Price": estimate_price_str,
                     "Selling price": price_number, "Currency": price_currency, "Reserve": reserve_or_not}
     else:
@@ -232,9 +233,11 @@ def check_data_none(price_sold, reserve_item, estimate_price):
 
     return price_number, price_currency, reserve_or_not, estimate_price_str
 
+
 def max_type_items(count_dict):
     """return the max out of two dictionary, is going to be used for the type of items """
     return max(count_dict, key=count_dict.get)
+
 
 def get_collection_data():
     """get all the items data point in addition to general info and specific info (art or object)
@@ -256,33 +259,27 @@ def get_collection_data():
     count_dict = {'Art pieces': 0, 'Other items': 0}
     sale_items = soup.find_all('div', class_='css-1up9enl')
 
-    # if the display is a list
     if check_type_display(sale_items):
-        # get the selling price, estimated price, currency used, reserve for each item
         for sale_item in sale_items:
             it = item_or_art_display_list(sale_item)
             price_sold = sale_item.find("p", class_="label-module_label12Medium__THkRn css-2r8rz8")
             estimate_price = sale_item.find_all("p", class_="paragraph-module_paragraph14Regular__Zfr98 css-trd9wg")[1]
             reserve_item = sale_item.find("p", class_="label-module_label12Medium__THkRn css-1hu9w0v")
 
-            price_number, price_currency, reserve_or_not, estimate_price_str = check_data_none(price_sold, reserve_item,                                                                                          estimate_price)
+            price_number, price_currency, reserve_or_not, estimate_price_str = check_data_none(price_sold, reserve_item,
+                                                                                               estimate_price)
 
-            # check if it is a art work (with 3 data points title, author and index)
-            # or an item (with 2 data points title and index) and return the data
             new_dict, count_dict = length_type_type(it, count_dict, estimate_price_str,
-                                        price_number, price_currency, reserve_or_not)
+                                                    price_number, price_currency, reserve_or_not)
 
             dict_items[it[0]] = new_dict
 
-    # if the display is made up of 'square'
     else:
         sale_items_2 = soup.find_all('div', class_="css-1esu0b4")
-        # get the selling price, estimated price, currency used, reserve for each item
         for sale_item in sale_items_2:
             it = item_or_art_display_square(sale_item)
             price_sold = sale_item.find("p", class_="label-module_label14Medium__uD9e- css-l21c39")
             estimate_price = sale_item.find_all("p", class_="paragraph-module_paragraph14Regular__Zfr98 css-trd9wg")[1]
-            # TODO i think it is the class for the list, need to check
             reserve_item = sale_item.find("p", class_="label-module_label12Medium__THkRn css-1xkt3wv")
 
             price_number, price_currency, reserve_or_not, estimate_price_str = check_data_none(price_sold, reserve_item,
@@ -305,11 +302,9 @@ def get_result_page_data():
     link_to_next_page = "https://www.sothebys.com/en/results?locale=en"
     page_index = 0
 
-    # for each result result page
     for page_number in range(NUMBER_OF_PAGES - 1):
         list_links, list_total_sales = get_url_n_sale_total()
 
-        # for each link present on the current page
         for link in list_links:
             driver.get(link)
             general_info()
@@ -317,7 +312,6 @@ def get_result_page_data():
             print(each_coll_dictionary)
             data_point_list.append(each_coll_dictionary)
 
-        # add to each dictionary the total sale volume of the auction
         index = 0
         while index < (len(list_total_sales) - 1):
             data_point_list[index + page_index]["Total Sale"] = list_total_sales[index]
