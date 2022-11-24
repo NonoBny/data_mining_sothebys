9567e8d25a55fe405ad5f525ae1fdebaa640313c
Yosef Schoen <josephaschoen@gmail.com>
2022-11-24 10:12:34 +0200

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	main.py
    #	new_page_scrapping.py

--- Objects_to_SQL/Utilility.py
+++ Objects_to_SQL/Utilility.py
@@ -5,15 +5,13 @@ import datetime
 connection = pymysql.connect(host='localhost',
                              user='root',
                              password='root',
-                             database='sothebys_db',
+                             database='collection_db',
                              charset='utf8mb4',
                              cursorclass=pymysql.cursors.DictCursor)
 
 
 class Column:
-    def __init__(self, name, data_type: str, options=None):
-        if options is None:
-            options = []
+    def __init__(self, name, data_type: str, options: List[str]):
         self.name = name
         self.data_type = data_type
         self.options = options
@@ -39,33 +37,30 @@ def get_date_time(date_str, time_str):
 def create_table(table_name: str, columns: List[Column], keys: List[str]):
     sql = "CREATE TABLE IF NOT EXISTS " + table_name + "(\n"
     for column in columns:
-        sql += column.name + ' ' + column.data_type + ' '
-
+        sql += column.name + ' ' + column.data_type
         for option in column.options:
-            sql += option + ' '
+            sql += ' ' + option
         sql += ','
+
+    sql = sql[:-1]
 
     for key in keys:
         sql += key + ','
-    sql = sql[:-1] + ')'
+    sql = sql[:-1]
 
-    print(sql)
     with connection.cursor() as cursor:
         cursor.execute(sql)
     return
 
 
-def insert_into_table(table_name: str, column_names: List[str], values: Tuple[str, ...]):
+def insert_into_table(table_name: str, columns: List[Column], values: Tuple[str, ...]):
     sql = "INSERT INTO " + table_name + "("
-    for column_name in column_names:
-        sql += column_name + ','
-    sql = sql[:-1] + ") "
+    for column in columns:
+        sql += column.name + ',\n'
+    sql = sql[:-1] + ")\n"
     sql += "VALUES (" + ("%s,"*len(values))
     sql = sql[:-1] + ")"
 
-    print(sql)
-    print(values)
     with connection.cursor() as cursor:
-        cursor.execute(sql, values)
-    connection.commit()
+        cursor.execute(sql)
     return
