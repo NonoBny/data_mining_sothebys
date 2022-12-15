3c6112f409d66004425a958f5cc641a0f4487251
Yosef <josephaschoen@gmail.com>
2022-12-15 09:28:16 +0200

    added aws db to config

--- Scrapers/Places_Scraper.py
+++ Scrapers/Places_Scraper.py
@@ -13,8 +13,8 @@ with open('config.json') as config_file:
     places_scraper_data = data["Places_Scraper_Data"]
 
 options = Options()
-#options.add_argument('--headless')
-#options.add_argument('--disable-gpu')
+options.add_argument('--headless')
+options.add_argument('--disable-gpu')
 driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
 
 
